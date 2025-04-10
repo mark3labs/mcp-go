ad295e3df1d3f1084b2efa7c4a538b3a17dfecb9
winter_wang <winter_wang@trendmicro.com>
2025-04-10 14:21:15 +0800

    increase readyCheckTimeout

--- client/stdio.go
+++ client/stdio.go
@@ -18,7 +18,7 @@ import (
 
 const (
 	readyTimeout      = 5 * time.Second
-	readyCheckTimeout = 100 * time.Millisecond
+	readyCheckTimeout = 3 * time.Second
 )
 
 // StdioMCPClient implements the MCPClient interface using stdio communication.
