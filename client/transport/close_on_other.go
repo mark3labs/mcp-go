2c494424e9c5e99d6bcf9f4e9bd0b536f857e5eb
Crayzero <chen154556138@163.com>
2025-04-21 16:59:14 +0800

    feat(transport/stdio.go): :sparkles: add send sigterm and sigkill to stdio client in close

---
+++ client/transport/close_on_other.go
@@ -0,0 +1,30 @@
+//go:build !windows
+// +build !windows
+
+package transport
+
+import (
+	"fmt"
+
+	"github.com/shirou/gopsutil/v3/process"
+)
+
+func killByPid(pid int) error {
+	proc, err := process.NewProcess(int32(pid))
+	if err != nil {
+		return err
+	}
+	// first send SIGTERM
+	err = proc.Terminate()
+	if err != nil {
+		fmt.Printf("Failed to send SIGTERM to pid %d: %v\n", pid, err)
+	}
+
+	_, err = process.Status()
+	if err == nil {
+		// kill ok
+		return nil
+	}
+	// send SIGKILL
+	return process.Kill()
+}
